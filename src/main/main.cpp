45ab87b62c063f14a0a1a0e934d8a77eb9b288ff
李响 <lostxine@gmail.com>
2016-11-15 15:37:13 +0800

    建立BWM项目测试.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,25 @@
 #include <tri_manager/tri_manager.h>
+#include <cstdio>
 
-int main()
+int main(int argc, char** argv)
 {
+/*测试代码*/
+/*
     tri_manager obj;
     obj.sayhello("world");
     obj.testOpenCV("path");
-    return 0;
+*/
+printf("---TRIANGULATION MANAGER---\nBuild time:%s %s\n",__TIME__,__DATE__);
+
+//打印参数环节：
+printf("---PRINT PARA---\nTotal para:%d\n",argc);
+for(int i = 0;i<argc;i++)
+{
+printf("Para %02d:%s\n",i,argv[i]);
+}
+
+//加载JSON环节
+
+
+return 0;
 }
