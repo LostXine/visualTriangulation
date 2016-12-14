4a9c55adfb537ac496f432321aba551874d086aa
李响 <lostxine@gmail.com>
2016-12-14 19:24:45 +0800

    修复了显示匹配点的错误，使用了更严格的滤波手段。

--- src/camera_unit/camera_unit.cpp
+++ src/camera_unit/camera_unit.cpp
@@ -18,7 +18,21 @@
 #endif
 using namespace rapidjson;
 
+int min (int a,int b){return a>b?b:a;}
+int max (int a,int b){return a>b?a:b;}
 
+//getCommonBox
+void getCommonBox(int* box1,int* box2,int *result)
+{
+	for(int i = 0;i<2;i++)
+	{
+		result[i] = min(box1[i],box2[i]) - 20;
+		result[i+2] = max(box1[i+2],box2[i+2]) + 20;
+	}
+	LOG(INFO)<<"x1:"<<result[0]<<" y1:"<<result[1]<<" x2:"<<result[2]<<" y2:"<<result[3];
+}
+
+//Vec3f/double
 cv::Vec3f cmod(cv::Vec3f p,double b)
 {
 	float q[3];
@@ -53,6 +67,7 @@ bool isIn(cv::Vec3f a,cv::Vec3b b,double radiu)
 {
 	return getDistance(a,b)<radiu;
 }
+
 //生成模型
 cv::Vec3f genModel(std::vector<cv::Vec3f> l)
 {
@@ -65,7 +80,7 @@ cv::Vec3f genModel(std::vector<cv::Vec3f> l)
 	return cmod(ret,l.size());
 }
 
-
+//IN/OUT/OUT/IN/IN
 int refilterPoints(cv::Mat result,std::vector<int>& mask,cv::Vec3f& ref,double radiu,double rate = 0.7)
 {
 	double thre = -rate;
@@ -103,26 +118,24 @@ int refilterPoints(cv::Mat result,std::vector<int>& mask,cv::Vec3f& ref,double r
 		if(m_thre>thre)
 		{
 			LOG(INFO)<<"INV:"<<inv<<" From "<<thre<<" to "<<m_thre;
-thre = m_thre;//移交最大值
-index = m_index;
-}
-inv++;
-}
+			thre = m_thre;//移交最大值
+			index = m_index;
+		}
+		inv++;
+	}
 //移交模板
-mask = index;
-std::vector<cv::Vec3f> flist;
-for(int i = 0;i<index.size();i++)
-{
-	flist.push_back(plist[index[i]]);
-}
-ref= genModel(flist);
+	mask = index;
+	std::vector<cv::Vec3f> flist;
+	for(int i = 0;i<index.size();i++)
+	{
+		flist.push_back(plist[index[i]]);
+	}
+	ref= genModel(flist);
 //计算距离
-if(thre>rate){return 0;}
-else
-{
-	return 1;
+	if(thre>rate){return 0;}
+	else{return 1;}
 }
-}
+
 
 
 
@@ -284,22 +297,22 @@ if(fs.isOpened())
 {
 	try
 	{
-	fs["K"]>>K;
-	LOG(INFO)<<"Load camera matrix:"<<std::endl<<K;
+		fs["K"]>>K;
+		LOG(INFO)<<"Load camera matrix:"<<std::endl<<K;
 	#ifdef DISTORTION_CORR
-	fs["dist"]>>distortion;
-	LOG(INFO)<<"Load camera distortion:"<<std::endl<<distortion;
+		fs["dist"]>>distortion;
+		LOG(INFO)<<"Load camera distortion:"<<std::endl<<distortion;
 	#endif
-	cv::Mat range;
-	fs["range"]>>range;
-	begin = range.at<double>(0,0);
-	end = range.at<double>(1,0);
-	LOG(INFO)<<"Begin:"<<begin<<" End:"<<end;
+		cv::Mat range;
+		fs["range"]>>range;
+		begin = range.at<double>(0,0);
+		end = range.at<double>(1,0);
+		LOG(INFO)<<"Begin:"<<begin<<" End:"<<end;
 	}
-catch(cv::Exception e)
-{
-	LOG(INFO)<<e.what();
-}
+	catch(cv::Exception e)
+	{
+		LOG(INFO)<<e.what();
+	}
 	fs.release();
 }
 else
@@ -326,31 +339,29 @@ camera_tri::~camera_tri()
 camera_single* camera_tri::addCamera(const char* js)
 {
 	camera_single* tcu = new camera_single(js,root);
-if(tcu->index<0){return 0;}//加载失败
-int idx = seq.size();
-if(idx > 0)
-{
-tcu->updateAbs(*(seq[idx-1]->getabs_T()),*(seq[idx-1]->getabs_R()));//更新坐标
-}
-else
-{
-//第一帧要初始化坐标
-	tcu->initAbs();
-}
-tcu->getCameraMat(K);//更新摄像机矩阵 
-seq.push_back(tcu);//输入队列
-//tcu->calculateKpt();//检测特征点
-tcu->loadImg();//读取图像
+	if(tcu->index<0){return 0;}//加载失败
+	int idx = seq.size();
+	if(idx > 0)
+	{
+		tcu->updateAbs(*(seq[idx-1]->getabs_T()),*(seq[idx-1]->getabs_R()));//更新坐标
+	}
+	else
+	{
+	//第一帧要初始化坐标
+		tcu->initAbs();
+	}
+	tcu->getCameraMat(K);//更新摄像机矩阵 
+	seq.push_back(tcu);//输入队列
+	//tcu->calculateKpt();//检测特征点
+	tcu->loadImg();//读取图像
 
-static bool gomatch = false;
-
-if(tcu->updateIsUsed(begin,end))//检查是否在范围内
-{
+	if(tcu->updateIsUsed(begin,end))//检查是否在范围内
+	{
 
 #ifdef DISTORTION_CORR
 	//Distortion correction
-	tcu->img_before_distortion = tcu->img.clone();
-	cv::undistort(tcu->img_before_distortion,tcu->img,K,distortion);
+		tcu->img_before_distortion = tcu->img.clone();
+		cv::undistort(tcu->img_before_distortion,tcu->img,K,distortion);
 	//display distorion
 	/*
 	std::vector<cv::Mat> chs;
@@ -364,47 +375,40 @@ if(tcu->updateIsUsed(begin,end))//检查是否在范围内
 #endif
 	//display done.
 #ifdef USE_GPU_SIFT
-	int width = tcu->img.cols;
-	int height = tcu->img.rows;
-	LOG(INFO)<<"W:"<<width<<" H:"<<height;
+		int width = tcu->img.cols;
+		int height = tcu->img.rows;
+		LOG(INFO)<<"W:"<<width<<" H:"<<height;
 //提取特征点
-	if(sift->RunSIFT(width, height, tcu->img.data, GL_INTENSITY8, GL_UNSIGNED_BYTE))
-	{
-		int num = sift->GetFeatureNum();
-		LOG(INFO)<<"Found SIFT num:"<<num;
-		tcu->kpts.resize(num);tcu->desp.resize(128*num);
-		sift->GetFeatureVector(&(tcu->kpts[0]),&(tcu->desp[0]));
-		LOG(INFO)<<"Extract Feature Vector Done";
-	}
+		if(sift->RunSIFT(width, height, tcu->img.data, GL_INTENSITY8, GL_UNSIGNED_BYTE))
+		{
+			int num = sift->GetFeatureNum();
+			LOG(INFO)<<"Found SIFT num:"<<num;
+			tcu->kpts.resize(num);tcu->desp.resize(128*num);
+			sift->GetFeatureVector(&(tcu->kpts[0]),&(tcu->desp[0]));
+			LOG(INFO)<<"Extract Feature Vector Done";
+		}
 #else
-	if(*(tcu->getBBox())>0)
-	{
-		int *bx = tcu->getBBox();
-		cv::Mat mk = cv::Mat::zeros(tcu->img.rows,tcu->img.cols,CV_8UC1);
-		cv::rectangle(mk,cv::Point(*bx,*(bx+1)),cv::Point(*(bx+2),*(bx+3)),cv::Scalar(255,255,255),CV_FILLED);
+		if(*(tcu->getBBox())>0)
+		{
+			int *bx = tcu->getBBox();
+			cv::Mat mk = cv::Mat::zeros(tcu->img.rows,tcu->img.cols,CV_8UC1);
+			cv::rectangle(mk,cv::Point(*bx,*(bx+1)),cv::Point(*(bx+2),*(bx+3)),cv::Scalar(255,255,255),CV_FILLED);
 /*
 cv::imshow("mask",mk);
 cv::waitKey(0);
 cv::destroyWindow("mask");
 */
-		detector->detect(tcu->img,tcu->kpts,mk);
-	}
-	else{detector->detect(tcu->img, tcu->kpts);}
-	descriptor->compute(tcu->img, tcu->kpts,tcu->desp);
-	LOG(INFO)<<"Found FAST num:"<<tcu->kpts.size();
+			detector->detect(tcu->img,tcu->kpts,mk);
+		}
+		else{detector->detect(tcu->img, tcu->kpts);}
+		descriptor->compute(tcu->img, tcu->kpts,tcu->desp);
+		LOG(INFO)<<"Found FAST num:"<<tcu->kpts.size();
 //tcu->drawKeypoints();
 #endif
-/*
-if(gomatch)
-{
-int p = seq.size();
-matchCamera(p-2,p-1);
-}
-gomatch = true;
-*/
-}
 
-return tcu;
+	}
+
+	return tcu;
 }
 
 double camera_tri::matchCamera(int l,int r,cv::Mat& result,cv::Mat& color)
@@ -421,112 +425,133 @@ double camera_tri::matchCamera(int l,int r,cv::Mat& result,cv::Mat& color)
 	}
 	LOG(INFO)<<"Left:"<<l <<" have "<<seq[l]->kpts.size()<<" pts.";
 	LOG(INFO)<<"Right:"<<r <<" have "<<seq[r]->kpts.size()<<" pts.";
-	std::vector<cv::DMatch>dm;
+	std::vector<cv::DMatch>rough_dmatch;
 	mh.push_back(lrmatch(l,r));
-	matcher->match(seq[l]->desp,seq[r]->desp, dm);
-	LOG(INFO)<<"Index "<<l<<" and "<<r<<" found "<<dm.size()<<" pairs";
-
-	std::vector<cv::Point2f>lp,rp;
-	if(dm.size()>10)
+	std::vector<lrmatch>::iterator this_match = mh.end()-1;
+	matcher->match(seq[l]->desp,seq[r]->desp, rough_dmatch);
+	LOG(INFO)<<"Index "<<l<<" and "<<r<<" found "<<rough_dmatch.size()<<" pairs";
+	
+	std::vector<cv::Point2f>point_in_left,point_in_right;
+	std::vector<int>index_vec_in_rough_dmatch;
+	if(rough_dmatch.size()>10)
 	{
-//匹配完毕，开始对齐
-		for(int i = 0;i<dm.size();i++)
+		//input all the match
+		for(int i = 0;i<rough_dmatch.size();i++)
 		{
-//(mh.end()-1)->match.push_back(dm[i]);
-			lp.push_back(seq[l]->kpts[dm[i].queryIdx].pt);
-			rp.push_back(seq[r]->kpts[dm[i].trainIdx].pt);
+			this_match->out_match.push_back(rough_dmatch[i]);
+		}
+
+//匹配完毕，开始对齐
+		for(int i = 0;i<rough_dmatch.size();i++)
+		{
+			point_in_left.push_back(seq[l]->kpts[rough_dmatch[i].queryIdx].pt);
+			point_in_right.push_back(seq[r]->kpts[rough_dmatch[i].trainIdx].pt);
 		}
 		cv::Mat mask;
-		cv::findFundamentalMat(lp,rp,CV_FM_RANSAC,1,0.99,mask);
+		cv::findFundamentalMat(point_in_left,point_in_right,CV_FM_RANSAC,1,0.99,mask);
 //滤除错配点
 //std::cout<<mask;
-		for(int p = 0;p<dm.size();p++)
+		for(int p = 0;p<rough_dmatch.size();p++)
 		{
 			if(mask.at<char>(p,0) == 1)
 			{
-				if(getDistance(seq[l]->kpts[dm[p].queryIdx].pt,seq[r]->kpts[dm[p].trainIdx].pt)<1e4)
+				if(getDistance(seq[l]->kpts[rough_dmatch[p].queryIdx].pt,seq[r]->kpts[rough_dmatch[p].trainIdx].pt)<1e5)
 				{
-					(mh.end()-1)->match.push_back(dm[p]);
+					this_match->match.push_back(rough_dmatch[p]);
+					index_vec_in_rough_dmatch.push_back(p);
 				}
 			}
+			
 		}
 	}
 	else
 	{
-		for(int p = 0;p<dm.size();p++)
+		LOG(WARNING)<<"rough_dmatch is too small to use fundamentalMat";
+		return -5;
+		/*
+		for(int p = 0;p<rough_dmatch.size();p++)
 		{
-			(mh.end()-1)->match.push_back(dm[p]);
-		}
+			this_match->match.push_back(rough_dmatch[p]);
+		}*/
 	}
-	LOG(INFO)<<"Index "<<l<<" and "<<r<<" found ("<<(mh.end()-1)->match.size()<<"/"<<dm.size()<<") pairs";
-	if((mh.end()-1)->match.size()<1){LOG(WARNING)<<"No matched pairs.";return -3;}
+	LOG(INFO)<<"Index "<<l<<" and "<<r<<" found ("<<this_match->match.size()<<"/"<<rough_dmatch.size()<<") pairs";
+	if(this_match->match.size()<1){LOG(WARNING)<<"No matched pairs.";return -3;}
 //重新对齐特征点
-	lp.clear();rp.clear();
+	point_in_left.clear();point_in_right.clear();
 
-	for(int i = 0;i<(mh.end()-1)->match.size();i++)
+	for(int i = 0;i<this_match->match.size();i++)
 	{
 //(mh.end()-1)->match.push_back(dm[i]);
-		lp.push_back(seq[l]->kpts[(mh.end()-1)->match[i].queryIdx].pt);
-		rp.push_back(seq[r]->kpts[(mh.end()-1)->match[i].trainIdx].pt);
+		point_in_left.push_back(seq[l]->kpts[this_match->match[i].queryIdx].pt);
+		point_in_right.push_back(seq[r]->kpts[this_match->match[i].trainIdx].pt);
 	}
 
-//LOG(INFO)<<lp.size()<<" "<<rp.size();
+//LOG(INFO)<<point_in_left.size()<<" "<<point_in_right.size();
 //LOG(INFO)<<seq[l]->getP()<<" "<<seq[r]->getP();
-	cv::Mat triRet;
-	cv::triangulatePoints(seq[l]->getP(),seq[r]->getP(),lp,rp,triRet);
+	cv::Mat result_of_triangulate_points;
+	cv::triangulatePoints(seq[l]->getP(),seq[r]->getP(),point_in_left,point_in_right,result_of_triangulate_points);
 //根据距离再滤一遍
-	std::vector<int> mkpt;
-cv::Vec3f posvec;//最终距离
-double distance = -1;
-if(!refilterPoints(triRet,mkpt,posvec,7,0.6))
-{
-	LOG(INFO)<<"After RANSAC: ("<<mkpt.size()<<"/"<<triRet.cols<<")";
-	distance = getDistance(*(seq[l]->getabs_T()),posvec);
-	cv::Mat rettmp (4,mkpt.size(),CV_32FC1);
-	lp.clear();
-	rp.clear();
-(mh.end()-1)->match.clear();//清除原有匹配
-for(int i = 0;i<mkpt.size();i++)
-{
-	int idx = mkpt[i];
-//写入显示
-	for(int j = 0;j<4;j++)
+	std::vector<int> remain_idx_after_distance_ransac;
+	cv::Vec3f distance_after_distance_ransac;//最终距离
+	double distance_return = -1;
+
+	if(!refilterPoints(result_of_triangulate_points,remain_idx_after_distance_ransac,distance_after_distance_ransac,15,0.6))
 	{
-		rettmp.at<float>(j,i) = triRet.at<float>(j,idx);
+		LOG(INFO)<<"After RANSAC: ("<<remain_idx_after_distance_ransac.size()<<"/"<<result_of_triangulate_points.cols<<")";
+		distance_return = getDistance(*(seq[l]->getabs_T()),distance_after_distance_ransac);
+		cv::Mat cloudpoint_result_tmp (4,remain_idx_after_distance_ransac.size(),CV_32FC1);
+		point_in_left.clear();
+		point_in_right.clear();
+		this_match->match.clear();//清除原有匹配
+		for(int i = 0;i<remain_idx_after_distance_ransac.size();i++)
+		{
+			int idx_in_triangulate_points = remain_idx_after_distance_ransac[i];
+	//写入显示
+			//cloudpoints
+			for(int j = 0;j<4;j++)
+			{
+				cloudpoint_result_tmp.at<float>(j,i) = result_of_triangulate_points.at<float>(j,idx_in_triangulate_points);
+			}
+			result = cloudpoint_result_tmp.clone();
+
+			//feature points
+			cv::DMatch* dmatch_tmp = &(rough_dmatch[index_vec_in_rough_dmatch[idx_in_triangulate_points]]);
+			this_match->match.push_back(*dmatch_tmp);//推入新的匹配
+			point_in_left.push_back(seq[l]->kpts[dmatch_tmp->queryIdx].pt);
+			point_in_right.push_back(seq[r]->kpts[dmatch_tmp->trainIdx].pt);
+		}
+
 	}
-	result = rettmp.clone();
-
-	cv::DMatch* dmtp = &(dm[idx]);
-(mh.end()-1)->match.push_back(*dmtp);//推入新的匹配
-lp.push_back(seq[l]->kpts[dmtp->queryIdx].pt);
-rp.push_back(seq[r]->kpts[dmtp->trainIdx].pt);
-}
-
-}
-else
-{
-//没有成功
-	LOG(INFO)<<"Fail to RANSAC.";
-	std::vector<cv::Vec3f>m_list;
-	for(int i = 0;i<triRet.cols;i++)
+	else
 	{
-		m_list.push_back(cv::Vec3f(triRet.at<float>(0,i)/triRet.at<float>(3,i),triRet.at<float>(1,i)/triRet.at<float>(3,i),triRet.at<float>(2,i)/triRet.at<float>(3,i)));
+	//没有成功
+		LOG(INFO)<<"Fail to RANSAC.";
+		return -4;
+		/*
+		std::vector<cv::Vec3f>m_list;
+		for(int i = 0;i<result_of_triangulate_points.cols;i++)
+		{
+			m_list.push_back(cv::Vec3f(result_of_triangulate_points.at<float>(0,i)/result_of_triangulate_points.at<float>(3,i),result_of_triangulate_points.at<float>(1,i)/result_of_triangulate_points.at<float>(3,i),result_of_triangulate_points.at<float>(2,i)/result_of_triangulate_points.at<float>(3,i)));
+		}
+		distance_after_distance_ransac = genModel(m_list);
+		distance_return = getDistance(*(seq[l]->getabs_T()),distance_after_distance_ransac);
+		result = result_of_triangulate_points.clone();
+		*/
+		
 	}
-	posvec = genModel(m_list);
-	distance = getDistance(*(seq[l]->getabs_T()),posvec);
-	result = triRet.clone();
-}
 
-color = cv::Mat(3,triRet.cols,CV_8UC1);//获取颜色
-for(int i = 0;i<color.cols;i++)
-{
-	color.at<char>(0,i) = seq[l]->img_color.at<cv::Vec3b>(lp[i])[2];
-	color.at<char>(1,i) = seq[l]->img_color.at<cv::Vec3b>(lp[i])[1];
-	color.at<char>(2,i) = seq[l]->img_color.at<cv::Vec3b>(lp[i])[0];
-}
 
-//drawMatch(l,r);
-return distance;
+	//get Color from origin pic.
+	color = cv::Mat(3,result_of_triangulate_points.cols,CV_8UC1);//获取颜色
+	for(int i = 0;i<color.cols;i++)
+	{
+		color.at<char>(0,i) = seq[l]->img_color.at<cv::Vec3b>(point_in_left[i])[2];
+		color.at<char>(1,i) = seq[l]->img_color.at<cv::Vec3b>(point_in_left[i])[1];
+		color.at<char>(2,i) = seq[l]->img_color.at<cv::Vec3b>(point_in_left[i])[0];
+	}
+
+	//drawMatch(l,r);
+	return distance_return;
 }
 
 
@@ -538,19 +563,34 @@ int camera_tri::drawMatch(int l,int r)
 		{
 			cv::Mat cm;
 			std::vector<cv::Mat>chs;
-			chs.push_back(seq[r]->img);
+			chs.push_back(seq[l]->img);
 			chs.push_back(seq[l]->img);
 			chs.push_back(seq[r]->img);
 			cv::merge(chs,cm);
+
+			for(int b = 0;b<seq[l]->kpts.size();b++)
+			{
+				cv::circle(cm,seq[l]->kpts[b].pt,3,cv::Scalar(255,0,128));
+			}
+			for(int b = 0;b<seq[r]->kpts.size();b++)
+			{
+				cv::circle(cm,seq[r]->kpts[b].pt,3,cv::Scalar(255,128,0));
+			}
+
+			for(int b = 0;b<mh[i].out_match.size();b++)
+			{
+				cv::line(cm,seq[l]->kpts[mh[i].out_match[b].queryIdx].pt,seq[r]->kpts[mh[i].out_match[b].trainIdx].pt,cv::Scalar(0,0,255));
+			}
 			for(int b = 0;b<mh[i].match.size();b++)
 			{
 				cv::line(cm,seq[l]->kpts[mh[i].match[b].queryIdx].pt,seq[r]->kpts[mh[i].match[b].trainIdx].pt,cv::Scalar(0,255,0));
 			}
-
-//cv::drawMatches(seq[l]->img_color,seq[l]->kpts,seq[r]->img_color,seq[r]->kpts,mh[i].match,cm);
-			cv::imshow("matches",cm);
+			int common_box[4];
+			getCommonBox(seq[l]->getBBox(),seq[r]->getBBox(),common_box);
+			cv::Rect roi_region(cv::Point(common_box[0],common_box[1]),cv::Point(common_box[2],common_box[3]));
+			cv::imshow("matches",cm(roi_region));
 			cv::waitKey(0);
-//cv::destroyWindow("matches");
+
 			return 0;
 		}
 	}
